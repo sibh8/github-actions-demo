4d07153fa12f85453b6336a97a9127629cd62a76
Bhandarkar <siddhesh.bhandarkar@nuance.com>
2021-08-09 23:33:19 +0530

    Adding a script to add a new issue

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,7 @@ try{
     const assignees = core.getInput('assignees');
 
     const octokit = github.getOctokit(token);
-    const response = octokit.rest.issues.addAssignees({
+    const response = octokit.issues.create({
                     owner: github.context.repo.owner,
                     repo: github.context.repo.repo,
                     issue_number: github.context.repo.issue_number,
