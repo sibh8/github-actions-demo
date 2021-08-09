1fd3f1b516b16369a70129cc3b35e80de9d2c53e
Bhandarkar <siddhesh.bhandarkar@nuance.com>
2021-08-09 20:22:45 +0530

    Adding the custom module action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -243,8 +243,11 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try{
-    
     // throw (new Error("Some error occurred"))
+
+    core.debug("This is a debug message"); //Executed only when ACTIONS_STEP_DEBUG is set to true
+    core.warning("This is a warning message"); //Executed only when ACTIONS_STEP_WARNING is set to true
+    core.error("This is a warning message"); //Executed only when ACTIONS_STEP_ERROR is set to true
 
     const name = core.getInput('who-to-greet');
     console.log(`Hello ${name}`);
@@ -252,7 +255,12 @@ try{
     const time = new Date();
     core.setOutput("time",time.toTimeString());
 
+    core.exportVariable('HELLO', 'hello');
+
+    core.startGroup("Logging Github object");
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
 } catch (error){
     core.setFailed(error.message)
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,8 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try{
-    
     // throw (new Error("Some error occurred"))
+
+    core.debug("This is a debug message"); //Executed only when ACTIONS_STEP_DEBUG is set to true
+    core.warning("This is a warning message"); //Executed only when ACTIONS_STEP_WARNING is set to true
+    core.error("This is a warning message"); //Executed only when ACTIONS_STEP_ERROR is set to true
 
     const name = core.getInput('who-to-greet');
     console.log(`Hello ${name}`);
@@ -11,7 +14,12 @@ try{
     const time = new Date();
     core.setOutput("time",time.toTimeString());
 
+    core.exportVariable('HELLO', 'hello');
+
+    core.startGroup("Logging Github object");
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
 } catch (error){
     core.setFailed(error.message)
 }
